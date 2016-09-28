559247cb21de684f3b5a2f6801d837da834d33ff
Bob <seedor87@students.rowan.edu>
2016-09-28 14:04:21 -0400

    added randomize in place to better reflect the runtime results of the two juxtaposed algos

--- Fun/partition_selection.py
+++ Fun/partition_selection.py
@@ -22,8 +22,16 @@ def exchange(List, a, b):
     List[a] = List[b]
     List[b] = temp
 
+def randomize_in_place(List):
+    for i in range(0, len(List)-1):
+        exchange(List, i, randint(i, len(List)-1))
+    return List
+
 @timer
 def qsort_selection(List, i):
+    """
+    This method involves first quick-sorting the list of values then indexing the required output for a solution
+    """
     return _qsort(List)[i-1]
 
 def _qsort(List):
@@ -37,6 +45,9 @@ def _qsort(List):
 
 @timer
 def partition_selection(List, i, p=None, r=None):
+    """
+    This method applies the partition investigation to the list to find the location of the i parameter smallest index
+    """
     return _partition_selection(List, p=0, r = len(List)-1, i=i)
 
 def _partition_selection(List, p, r, i):
@@ -60,18 +71,15 @@ def partition(List, p, r):
     exchange(List, p, r)
     return p
 
-lim = 1000
-primes = [1,2]
-for num in range(3,lim,2):
-    if all(num%i!=0 for i in range(2,int(math.sqrt(num))+1)):
-       primes.append(num)
+"""All Prime Numbers Under 1000"""
+primes = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
 
 def main():
-    input = primes
+    input = randomize_in_place(primes)
     print "\nInput: %s\nEnter a choice of ith smallest number, or q to quit--\n" % (input)
     while True:
         try:
-            n = raw_input(">> " % (input))
+            n = raw_input(">> ")
             if n == "q":
                 print "exiting"
                 break
