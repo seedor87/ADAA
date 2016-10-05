fe488bfdfe21c8c942a1be15b3c7d0ec69564718
Bob <seedor87@students.rowan.edu>
2016-10-05 17:30:28 -0400

    Fixed algo and test set for problem 1 of hw, see changes in 8.2-4.p

--- HW3/8.2-4.py
+++ HW3/8.2-4.py
@@ -1,3 +1,6 @@
+from random import randint
+
+list_rand_int = lambda Lim, Len, Sign=0: [randint(Sign*Lim,Lim) for x in range(0,Len)]
 
 def actual_solution(List, a, b, k=10):
 
@@ -14,53 +17,15 @@ def actual_solution(List, a, b, k=10):
 
         return C[b] - C[a-1]
 
-def solution_non_negatives(List, a, b, k=10):
-
-    if a <= b:
-        count = [0] * (k + 1)
-
-        for i in List:
-            count[i] += 1
-
-        return sum(count[a:b+1]), zip(xrange(a, b+1), count[a:b+1])
-    else:
-        raise Exception('Invalid Indices')
-
-def solution_allows_negatives(List, a, b, k=10):
-
-    if a <= b:
-        count ={}
-        for i in range(-k, k):
-            count[i] = 0
-
-        for i in List:
-            count[i] += 1
-
-        res = 0
-        ret = []
-        for i in range(a, b+1):
-            res += count[i]
-            ret.append((i, count[i]))
-        return res, ret
-    else:
-        raise Exception('Invalid Indices, %s not <= %s' % (a, b))
-
-# try:
-#     array = [8,7,4,5,3,1,5]
-#     print solution_non_negatives(array, 3, 2)
-# except Exception as e:
-#     print e
 
 array = [8,7,4,5,3,1,5]
-print solution_non_negatives(array, 1, 6)
 print actual_solution(array, 1, 6)
 
-# array = [-1,1,-2,-3,3,2,1]
-# print solution_allows_negatives(array, -2, 2)
-#
-# array = [-62, -32, 89, 72, -76, -11, 54, -32, -63, 12, 11, 60, 32, -27, 32, 17, -85, -49, -3, 27, -71, 52, -21, -49, 5, -12, 4, -67, 45, 54, -55, 84, 74, 78, -32, 23, 74, 73, 57, 81, -5, 98, 51, 18, 73, -90, 10, -71, 41, -93, -92, 36, -26, -94, -33, -75, 55, 55, 18, 29, -39, -15, -70, 85, -35, 20, 89, -93, -63, -46, 70, 60, -82, -86, 41, 40, 73, 38, 28, 35, -87, -72, 8, 7, -26, -97, 5, -44, 83, 66, -87, 82, 83, -65, 14, -87, 19, -78, 69, -4]
-# print solution_allows_negatives(array, -50, 50, k=100)
+array = [0,0,1,2,5,7,11,17,21,56]
+print actual_solution(array, 12, 16, k=100)
+print actual_solution(array, 11, 56, k=100)
 
-
+array = [18, 81, 37, 3, 76, 44, 79, 92, 62, 55, 27, 28, 0, 28, 16, 5, 11, 29, 6, 38, 21, 45, 90, 65, 81, 24, 92, 40, 60, 65, 48, 16, 32, 27, 39, 38, 57, 38, 18, 11, 14, 24, 82, 68, 15, 31, 40, 63, 12, 72, 82, 57, 67, 74, 35, 35, 85, 46, 29, 73, 75, 81, 86, 26, 1, 53, 33, 5, 32, 1, 71, 7, 66, 34, 6, 89, 27, 50, 4, 15, 28, 52, 98, 37, 93, 11, 42, 73, 22, 77, 25, 99, 47, 58, 97, 95, 47, 84, 71, 80]
+print actual_solution(array, 10, 30, k=100)
 
 
