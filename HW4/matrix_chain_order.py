36b4a48cefd8465c3b28134cd1d5d2e6a2f5169b
Bob <seedor87@students.rowan.edu>
2016-10-27 19:15:47 -0400

    added working solution to question 2

---
+++ HW4/matrix_chain_order.py
@@ -0,0 +1,81 @@
+from __future__ import division
+import sys, timeit
+from random import randint
+from functools import wraps
+import matplotlib.pyplot as plt
+from pprint import pprint
+
+list_rand_int = lambda Lim, Len, Sign=0: [randint(Sign*Lim,Lim) for x in range(0,Len)]
+
+def wrap_text(text, highlight=None):
+    if highlight is None:
+        return str(text)
+    else:
+        return '%s%s%s' %(highlight, text, color.END)
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+def timer(function):
+
+    @wraps(function)
+    def func_timer(*args, **kwargs):
+        t0 = timeit.default_timer()
+        result = function(*args, **kwargs)
+        t1 = timeit.default_timer()
+        diff = t1 -t0
+        print ("Total time running %s: %s seconds" %(wrap_text(function.func_name, color.DARKCYAN), wrap_text(diff, color.YELLOW)))
+        return result, diff
+    return func_timer
+
+
+@timer
+def matrix_chain_order(p):
+
+    def print_optimal_parenthization(s, i, j):
+        if i == j:
+            return "A%s" % i
+        else:
+            k = s[i][j]
+            left = print_optimal_parenthization(s, i, k)
+            right = print_optimal_parenthization(s, k+1, j)
+            return "(%s * %s)" % (left, right)
+
+    n = len(p)
+    m = [[0 for x in range(n)] for x in range(n)]
+    s = [[0 for x in xrange(n)] for x in xrange(n)]
+
+    for i in range(1, n):
+        m[i][i] = 0
+
+    for L in range(2, n):
+        for i in range(1, n - L + 1):
+            j = i + L - 1
+            m[i][j] = sys.maxint
+
+            for k in range(i, j):
+                q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j]
+                if q < m[i][j]:
+                    m[i][j] = q
+                    s[i][j] = k
+
+    print print_optimal_parenthization(s, 1, n-1)
+    pprint(s)
+    return m[1][n-1]
+
+def main3():
+
+    input = [5, 10, 3, 12, 5, 50]
+    print matrix_chain_order(input)
+
+if __name__ == '__main__':
+    main3()
