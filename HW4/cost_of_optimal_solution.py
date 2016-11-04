1ed490bdc86b340330ce0e1a94280c6a4618599a
Bob <seedor87@students.rowan.edu>
2016-11-04 17:08:58 -0400

    adding work to q4 hw4, iterative solution works, but may not  be what he is seeking

--- HW4/cost_of_optimal_solution.py
+++ HW4/cost_of_optimal_solution.py
@@ -25,30 +25,35 @@ def solution(x, y):
     while len(x) > 0 and len(y) > 0:
         at_x, at_y = x.pop(0), y.pop(0)
         if at_x == at_y:
-            print "Pass Down\t\t", at_y, 0
+            print "Pass Down\t\t", 0, at_y, y
             pass
         else:
             if len(x) > 0 and len(y) > 0:
                 if x[0] == at_y and at_x == y[0]:
-                    print "Twiddle 1\t\t", at_x, 0.5
-                    print "Twiddle 2\t\t", at_y, 0.5
+                    print "Twiddle\t\t", 0.5, at_x, at_y, y
                     x.pop(0)
                     y.pop(0)
                     total += 1
             else:
-                print "Drop / Add", at_y, "->", at_x, 2
+                print "Drop / Add", at_y, "->", 2, at_x, y
                 total += 2
     if len(x) > len(y):
-        for char in x:
-            print "Insert from X\t", char, 1
+        while len(x) > 0:
+            at_x = x.pop(0)
+            print "Insert from X\t", 1, at_x, x
             total += 1
     elif len(y) > len(x):
-        for char in y:
-            print "Delete from Y\t", char, 1
+        while len(y) > 0:
+            at_y = y.pop(0)
+            print "Delete from Y\t",1, at_y, y
             total += 1
     else:
         pass
     print "\t\t\t Tot:", total
+
+x = ""
+y = "Ha"
+solution(list(x), list(y))
 
 x = "He"
 y = "Ha"
